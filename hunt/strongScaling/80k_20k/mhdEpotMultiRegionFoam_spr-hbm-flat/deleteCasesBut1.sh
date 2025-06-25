282e9300f9eb81e22809af783d4a8a3588312471
Rupert Eardley-Brunt <rupert.eardley-brunt@ukaea.uk>
2025-06-25 10:53:29 +0100

    Add scaling run generation script for spr-hbm-flat

---
+++ hunt/strongScaling/80k_20k/mhdEpotMultiRegionFoam_spr-hbm-flat/deleteCasesBut1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+coresAllBut1=(2 4 8 14 28 56 112 224 448)
+
+for cores in ${coresAllBut1[@]}
+do
+  	dir="P_$cores"
+        if [[ -d $dir ]]
+        then
+                rm -r $dir
+        else
+            	echo "Directory $dir does not exist!"
+        fi
+done
+
